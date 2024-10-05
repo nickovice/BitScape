f6c0d361416c4feb8d865fdddb48e9020455fb86
nickovice <nicolasviceconti@gmail.com>
2024-10-05 03:39:46 -0300

    initial commit v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from '../app/products/products.component';
+import { AboutComponent } from '../app/about/about.component';
+import { HomeComponent } from '../app/home/home.component';
+import { AdminComponent } from '../app/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '',redirectTo: 'home', pathMatch: 'full' },{ path: 'home', component: HomeComponent },{ path: 'products', component: ProductsComponent },
+  { path: 'about', component: AboutComponent }, { path: 'admin', component: AdminComponent }, { path: '**', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductsComponent,
+    HomeComponent,
+    AdminComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
