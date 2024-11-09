fe334c5219dc69c3d327100b44a6f0fbc36f1770
nickovice <nicolasviceconti@gmail.com>
2024-11-08 22:17:50 -0300

    testing vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import { inject } from "@vercel/analytics"
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+
+inject();
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
