e0d6d3cb7776a10be16dfebb20ac561df23008a6
nickovice <nicolasviceconti@gmail.com>
2024-11-08 22:34:32 -0300

    more analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 inject();
+injectSpeedInsights();
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
